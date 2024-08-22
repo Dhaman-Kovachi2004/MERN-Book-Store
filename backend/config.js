bfad9653e06d5ab1017c8a8f7851c9967f88bd52
Dhaman Kovachi <dhamankovachi1@gmail.com>
2024-08-22 23:12:55 +0530

    changes made in config.js and created .env

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,5 @@
-export const PORT =5555;
+import 'dotenv/config'
+export const PORT =process.env.PORT || 5555;
 
-export const mongodbURL="mongodb://root:root@book-store-mern-shard-00-00.z5w15.mongodb.net:27017,book-store-mern-shard-00-01.z5w15.mongodb.net:27017,book-store-mern-shard-00-02.z5w15.mongodb.net:27017/?ssl=true&replicaSet=atlas-9k78t0-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Book-Store-MERN"
+export const mongodbURL=process.env.MONGO_URL
+
